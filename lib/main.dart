0ff9959745b9067ba6c574e66015ae00c8c5a40e
mehmetjank <211216058@stu.istinye.edu.tr>
2023-05-12 22:13:22 +0300

    First Commit
    The basic part of the application has been build.

---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'bloc/settings/settings_cubit.dart';
+import 'bloc/settings/settings_state.dart';
+import 'localizations/localizations.dart';
+import 'routes/route.dart';
+import 'themes/theme.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => SettingsCubit(SettingsState()),
+      child:
+          BlocBuilder<SettingsCubit, SettingsState>(builder: (context, state) {
+        return MaterialApp.router(
+          title: 'Starter',
+          debugShowCheckedModeBanner: false,
+          localizationsDelegates: const [
+            AppLocalizations.delegate,
+            // GlobalMaterialLocalizations.delegate,
+            // GlobalCupertinoLocalizations.delegate,
+            // GlobalWidgetsLocalizations.delegate,
+          ],
+          supportedLocales: AppLocalizations.supportedLanguages
+              .map((e) => Locale(e, ""))
+              .toList(),
+          locale: Locale(state.language, ""),
+          themeMode: state.darkMode ? ThemeMode.dark : ThemeMode.light,
+          theme: Themes.lightTheme,
+          darkTheme: Themes.darkTheme,
+          routerConfig: routes,
+        );
+      }),
+    );
+  }
+}
---
+++ lib/routes/route.dart
@@ -0,0 +1,55 @@
+import 'package:go_router/go_router.dart';
+
+final routes = GoRouter(
+  initialLocation: '/',
+  routes: [
+    // Loader Screen
+    // GoRoute(
+    //   path: '/',
+    //   builder: (context, state) => InitialScreen(),
+    // ),
+    // GoRoute(
+    //   path: '/welcome',
+    //   builder: (context, state) => WelcomeScreen(),
+    // ),
+    // GoRoute(
+    //   path: '/home',
+    //   builder: (context, state) => HomeScreen(),
+    // ),
+    // // User Screens
+    // GoRoute(
+    //   path: '/profile',
+    //   builder: (context, state) => ProfileScreen(),
+    // ),
+    // GoRoute(
+    //   path: '/login',
+    //   builder: (context, state) => LoginScreen(),
+    // ),
+    // GoRoute(
+    //   path: '/register',
+    //   builder: (context, state) => RegisterScreen(),
+    // ),
+    // GoRoute(
+    //   path: '/change_password',
+    //   builder: (context, state) => ChangePasswordScreen(),
+    // ),
+    // GoRoute(
+    //   path: '/forget_password',
+    //   builder: (context, state) => ForgetPasswordScreen(),
+    // ),
+
+    // // Static Screen
+    // GoRoute(
+    //   path: '/about',
+    //   builder: (context, state) => AboutScreen(),
+    // ),
+    // GoRoute(
+    //   path: '/contact',
+    //   builder: (context, state) => ContactScreen(),
+    // ),
+    // GoRoute(
+    //   path: '/settings',
+    //   builder: (context, state) => SettingsScreen(),
+    // ),
+  ],
+);
